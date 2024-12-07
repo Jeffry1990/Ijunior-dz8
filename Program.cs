65d39aabb43e292f7513cc8889f630755a260f39
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2024-12-08 01:16:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp47
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialValue = 5;
+            int maximumValue = 103;
+            int step = 7;
+
+            for (int i = initialValue; i <= maximumValue; i += step)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
